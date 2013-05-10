339bc4f80fabc07f79d89f0999bdeb2cc4d55772
John Tsiombikas <nuclear@mutantstargoat.com>
2013-05-10 09:50:00 +0300

    added copyright header to the source

--- src/shadertoy.cc
+++ src/shadertoy.cc
@@ -1,3 +1,20 @@
+/*
+A native shadertoy-compatible GLSL fragment shader previewer.
+Copyright (C) 2013  John Tsiombikas <nuclear@member.fsf.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
@@ -127,7 +144,6 @@ int main(int argc, char **argv)
 	for(size_t i=0; i<tex_arg.size(); i++) {
 		activetex[i] = textures[tex_arg[i]];
 	}
-
 
 	if(!(sdr = load_shader(sdrfname_arg))) {
 		return 1;
